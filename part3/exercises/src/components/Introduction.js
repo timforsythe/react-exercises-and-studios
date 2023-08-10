6e6e5acb121a4b866583229f786f7aaaf2db2a61
forsythet@gmail.com <forsythet@gmail.com>
2023-08-10 15:38:44 -0500

    Added bread images

---
+++ part3/exercises/src/components/Introduction.js
@@ -0,0 +1,12 @@
+export default function HobbyIntroduction () {
+    return(
+        <div>
+            <h2>Three Fun Facts About Baking Bread</h2>
+            <ol>
+                <li>Bread is primarily composed of 4 simple ingredients: flour, water, salt, and yeast.</li>
+                <li>It can sometimes take over 18 hours to make just one loaf of bread.</li>
+                <li>Anyone can make bread, no matter their age.</li>
+            </ol>
+        </div>
+    );
+ }
